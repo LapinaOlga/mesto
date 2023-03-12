5f32cc85e21e46d72dd468ae715c0e01c5ada0a1
Lapina Olga <osotskova1992@icloud.com>
2023-03-12 14:11:35 +0100

    Реализованы карточки и лайки

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,145 @@
 'use strict';
 
 const profileEditButton = document.querySelector('.profile__edit-button');
-
-const popup = document.querySelector('.popup');
-
-const popupIconClose = document.querySelector('.popup__icon-close');
-
+const profileButton = document.querySelector('.profile__button');
+const profilePopup = document.querySelector('.profile-popup');
+const cardPopup = document.querySelector('.card-popup');
+const profilePopupIconClose = profilePopup.querySelector('.popup__icon-close');
+const cardPopupIconClose = cardPopup.querySelector('.popup__icon-close');
 const inputUserName = document.querySelector('input.popup__item_el_name');
-
 const profileTitle = document.querySelector('.profile__title');
-
 const inputUserProfession = document.querySelector('input.popup__item_el_profession');
-
 const profileSubtitle = document.querySelector('.profile__subtitle');
+const profileForm = document.querySelector('.profile-popup form');
+const cardForm = document.querySelector('.card-popup form');
+const inputLocation = document.querySelector('input.popup__item_el_location');
+const inputLink = document.querySelector('input.popup__item_el_link');
 
-const profileForm = document.querySelector('form');
 
-function closePopup() {
-    popup.classList.remove('popup_opened');
+// Общая функция закрытия попапа
+function closePopup(popupClassName) {
+    popupClassName.classList.remove('popup_opened');
 }
+
+// Общая функция открытия попапа
+function openPopup(popupClassName) {
+    popupClassName.classList.add('popup_opened');
+}
+
+//Попап для редактирования профиля
+
 profileForm.addEventListener('submit', function (event) {
     event.preventDefault();
     profileTitle.textContent = inputUserName.value;
     profileSubtitle.textContent = inputUserProfession.value;
-    closePopup();
+    closePopup(profilePopup);
 });
 
+// Открываем попап и добавляем текст из профиля в попап
 profileEditButton.addEventListener('click', function () {
-    popup.classList.add('popup_opened');
+    openPopup(profilePopup);
     inputUserName.value = profileTitle.textContent;
     inputUserProfession.value = profileSubtitle.textContent;
 });
-
-popupIconClose.addEventListener('click', function () {
-    closePopup();
+// Закрываем попап
+profilePopupIconClose.addEventListener('click', function () {
+    closePopup(profilePopup);
 });
+
+
+//Попап для создания карточки
+
+profileButton.addEventListener('click', function () {
+    openPopup(cardPopup);
+});
+
+cardPopupIconClose.addEventListener('click', function () {
+    closePopup(cardPopup);
+});
+
+
+// Создаем карточки
+const cardElements = document.querySelector('.elements');
+const cardTemplate = document.querySelector('#elements').content;
+const cardElement = cardTemplate.querySelector('.element');
+const cardImage = cardElement.querySelector('.element__image');
+const cardArea = cardElement.querySelector('.element__area');
+const cardTitle = cardArea.querySelector('.element__title');
+
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+// Добавление карточек
+
+function addCard(card) {
+    cardImage.src = card.link;
+    cardTitle.textContent = card.name;
+    const newCard = cardElement.cloneNode(true);
+    cardElements.prepend(newCard);
+
+    // Реализуем лайки
+    const addLike = newCard.querySelector('.element__button')
+    addLike.addEventListener('click', function (event) {
+        event.target.classList.toggle('element__button_active');
+    })
+}
+
+for (let i = 0; i < initialCards.length; i++) {
+    addCard(initialCards[i])
+}
+
+// Создание новой карточки из попапа
+cardForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    addCard({
+        link: inputLink.value,
+        name: inputLocation.value,
+    });
+
+    const cards = cardElements.querySelectorAll('.element');
+    cards[cards.length-1].remove()
+
+
+    closePopup(cardPopup);
+    inputLink.value = null
+    inputLocation.value = null
+
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
 
